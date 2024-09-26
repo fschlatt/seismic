6a695e27f61f0cf30afc09aab6b882f290dac9ab
CosimoRulli <cosimo.rulli@gmail.com>
2024-09-26 14:37:44 +0000

    added interface for u32 coordinates

--- src/pylib/mod.rs
+++ src/pylib/mod.rs
@@ -1,10 +1,12 @@
+use crate::inverted_index::{
+    BlockingStrategy, Configuration, PruningStrategy, SummarizationStrategy,
+};
 use crate::{InvertedIndex, SparseDataset};
 use half::f16;
 use numpy::PyReadonlyArrayDyn;
 use pyo3::prelude::*;
 use rayon::prelude::*;
 use std::fs;
-use crate::inverted_index::{BlockingStrategy, Configuration, PruningStrategy, SummarizationStrategy};
 
 #[pyclass]
 pub struct PySeismicIndex {
@@ -30,13 +32,15 @@ impl PySeismicIndex {
 
     #[staticmethod]
     #[allow(clippy::too_many_arguments)]
-    pub fn build(input_file: &str,
-                 n_postings: usize,
-                 centroid_fraction: f32,
-                 truncated_kmeans_training: bool,
-                 truncation_size: usize,
-                 min_cluster_size: usize,
-                 summary_energy: f32) -> PyResult<PySeismicIndex> {
+    pub fn build(
+        input_file: &str,
+        n_postings: usize,
+        centroid_fraction: f32,
+        truncated_kmeans_training: bool,
+        truncation_size: usize,
+        min_cluster_size: usize,
+        summary_energy: f32,
+    ) -> PyResult<PySeismicIndex> {
         let dataset = SparseDataset::<u16, f32>::read_bin_file(input_file)
             .unwrap()
             .quantize_f16();
@@ -52,9 +56,7 @@ impl PySeismicIndex {
                 truncation_size,
                 min_cluster_size,
             })
-            .summarization_strategy(SummarizationStrategy::EnergyPerserving {
-                summary_energy,
-            });
+            .summarization_strategy(SummarizationStrategy::EnergyPerserving { summary_energy });
         println!("\nBuilding the index...");
         println!("{:?}", config);
 
@@ -103,13 +105,112 @@ impl PySeismicIndex {
         queries
             .par_iter()
             .map(|query| {
-                self.inverted_index.search(
-                    query.0,
-                    query.1,
-                    k,
-                    query_cut,
-                    heap_factor,
-                )
+                self.inverted_index
+                    .search(query.0, query.1, k, query_cut, heap_factor)
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
+#[pyclass]
+pub struct PySeismicIndexLargeVocabulary {
+    inverted_index: InvertedIndex<u32, f16>,
+}
+
+#[pymethods]
+impl PySeismicIndexLargeVocabulary {
+    #[staticmethod]
+    pub fn load(index_path: &str) -> PyResult<PySeismicIndexLargeVocabulary> {
+        let serialized: Vec<u8> = fs::read(index_path).unwrap();
+        let inverted_index = bincode::deserialize::<InvertedIndex<u32, f16>>(&serialized).unwrap();
+        Ok(PySeismicIndexLargeVocabulary { inverted_index })
+    }
+
+    pub fn save(&self, path: &str) {
+        let serialized = bincode::serialize(&self.inverted_index).unwrap();
+        let path = path.to_string() + ".index.seismic";
+        println!("Saving ... {}", path);
+        let r = fs::write(path, serialized);
+        println!("{:?}", r);
+    }
+
+    #[staticmethod]
+    #[allow(clippy::too_many_arguments)]
+    pub fn build(
+        input_file: &str,
+        n_postings: usize,
+        centroid_fraction: f32,
+        truncated_kmeans_training: bool,
+        truncation_size: usize,
+        min_cluster_size: usize,
+        summary_energy: f32,
+    ) -> PyResult<PySeismicIndexLargeVocabulary> {
+        let dataset = SparseDataset::<u32, f32>::read_bin_file(input_file)
+            .unwrap()
+            .quantize_f16();
+
+        let config = Configuration::default()
+            .pruning_strategy(PruningStrategy::GlobalThreshold {
+                n_postings,
+                max_fraction: 1.5,
+            })
+            .blocking_strategy(BlockingStrategy::RandomKmeans {
+                centroid_fraction,
+                truncated_kmeans_training,
+                truncation_size,
+                min_cluster_size,
+            })
+            .summarization_strategy(SummarizationStrategy::EnergyPerserving { summary_energy });
+        println!("\nBuilding the index...");
+        println!("{:?}", config);
+
+        let inverted_index = InvertedIndex::build(dataset, config);
+        Ok(PySeismicIndexLargeVocabulary { inverted_index })
+    }
+
+    pub fn search<'py>(
+        &self,
+        query_components: PyReadonlyArrayDyn<'py, i32>,
+        query_values: PyReadonlyArrayDyn<'py, f32>,
+        k: usize,
+        query_cut: usize,
+        heap_factor: f32,
+    ) -> Vec<(f32, usize)> {
+        self.inverted_index.search(
+            &query_components
+                .to_vec()
+                .unwrap()
+                .iter()
+                .map(|x| *x as u32)
+                .collect::<Vec<_>>(),
+            &query_values.to_vec().unwrap(),
+            k,
+            query_cut,
+            heap_factor,
+        )
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub fn batch_search<'py>(
+        &self,
+        query_path: &str,
+        k: usize,
+        query_cut: usize,
+        heap_factor: f32,
+        num_threads: usize,
+    ) -> Vec<Vec<(f32, usize)>> {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build()
+            .unwrap();
+
+        let queries = SparseDataset::<u32, f32>::read_bin_file(query_path).unwrap();
+
+        queries
+            .par_iter()
+            .map(|query| {
+                self.inverted_index
+                    .search(query.0, query.1, k, query_cut, heap_factor)
             })
             .collect::<Vec<_>>()
     }
